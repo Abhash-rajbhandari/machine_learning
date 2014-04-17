c818e01e88ac96c1426baf850cfb558c340f46dd
Abhash <rajbhandari.abhash@gmail.com>
2014-04-17 21:40:16 +0545

    Splitting Python Code

---
+++ split.py
@@ -0,0 +1,23 @@
+import codecs
+import re
+infile = codecs.open("27_51066.txt", "r", "utf8")
+outfile = codecs.open("output.csv", "w", "utf8")
+file = infile.read()
+end = re.compile(ur'([\u0964]\s|[\u003F]\s|[\u0021]\s)', re.UNICODE)
+
+split_sent = end.split(file)
+
+i = 0
+while(i<len(split_sent)):
+    outfile.write(split_sent[i])
+
+    if (len(split_sent[i])<3):
+        outfile.write("\n")
+    i = i + 1
+
+print("Done. Please check the file output.txt",i)
+
+
+
+
+
